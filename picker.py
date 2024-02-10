5fa2dcad79e31f46a2e7b347d0d4c1169f194f19
Dorion Boesch <dorion.boesch@gmail.com>
2024-02-09 19:20:01 -0700

    first commit

---
+++ picker.py
@@ -0,0 +1,20 @@
+import random
+
+char_list = [
+    'Asuka R#',
+    'Baiken',
+    'Bridget',
+    'Elphelt',
+    'Giovanna',
+    'Happy Chaos',
+    'I-No',
+    'Jack-O',
+    'May',
+    'Millia',
+    'Nagoriyuki',
+    'Ramlethal',
+    'Testament',
+    'Variety',
+    ]
+
+print(random.choice(char_list))
